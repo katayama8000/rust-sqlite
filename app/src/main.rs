ab384f32487ee0f9f5dcaef046599a34e0a683ed
katayama800 <tattu.0310@gmail.com>
2023-12-06 14:14:55 +0000

    expo push api

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,31 @@
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
+use serde_json::json;
+use tokio;
 
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let url = "https://exp.host/--/api/v2/push/send";
+    let expo_token = "ExponentPushToken[TOWNJ5LmG02dzppStD58kK]";
 
-fn main() {
-    println!("Hello, world!");
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+
+    let client = reqwest::Client::new();
+
+    let payload = json!({
+        "to": expo_token,
+        "title": "hello",
+        "body": "rust",
+    });
+
+    let response = client
+        .post(url)
+        .headers(headers)
+        .json(&payload)
+        .send()
+        .await?;
+
+    println!("{:?}", response.text().await?);
+
+    Ok(())
 }
