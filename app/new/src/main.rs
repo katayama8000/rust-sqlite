323fd9c719f2c67445c1f82d8f3bd83de71f342f
katayama800 <tattu.0310@gmail.com>
2024-03-02 10:09:16 +0000

    fix dir

--- app/new/src/main.rs
+++ app/new/src/main.rs
@@ -4,9 +4,15 @@ async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn get_data_from_db() -> &'static str {
+    "Hello, world!!!!!"
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/get", get(get_data_from_db));
 
     Ok(router.into())
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -2,7 +2,7 @@ use dotenv;
 use sqlx::sqlite::SqlitePool;
 use std::env;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct User {
     pub id: i64,
     pub name: String,
@@ -11,20 +11,54 @@ pub struct User {
     pub created_at: chrono::NaiveDateTime,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    dotenv::dotenv().expect("Failed to read .env file");
+use axum::{routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn get_data_from_db() -> String {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = SqlitePool::connect(&database_url).await?;
+    let pool = SqlitePool::connect(&database_url).await.unwrap();
     let users = sqlx::query_as!(
         User,
         "select id, name, email, address, created_at from users"
     )
     .fetch_all(&pool)
-    .await?;
-    for user in users {
+    .await
+    .unwrap();
+    for user in users.clone() {
         println!("{:?}", user);
     }
 
-    Ok(())
+    users[0].name.clone()
 }
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    dotenv::dotenv().expect("Failed to read .env file");
+
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/get", get(get_data_from_db));
+
+    Ok(router.into())
+}
+
+// #[tokio::main]
+// async fn main() -> Result<(), sqlx::Error> {
+//     dotenv::dotenv().expect("Failed to read .env file");
+//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     let pool = SqlitePool::connect(&database_url).await?;
+//     let users = sqlx::query_as!(
+//         User,
+//         "select id, name, email, address, created_at from users"
+//     )
+//     .fetch_all(&pool)
+//     .await?;
+//     for user in users {
+//         println!("{:?}", user);
+//     }
+
+//     Ok(())
+// }
